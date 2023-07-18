bb24d2692ad36e2a3326f4696a04c9873a1abb47
Adam Olson <adam.lee.olson@outlook.com>
2023-07-17 21:19:45 -0500

    trying to pull from main

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 //the call to bored API
+var mainButton = document.querySelector(".button")
 var dropdownHeader = document.querySelector(".dropbtn")
 var dropdownOptions = document.querySelector(".dropdown-content")
+var boredAPIResponse = document.querySelector(".boredAPI")
+var GIFEl = document.querySelector(".giphy-gif")
 
-fetch("https://www.boredapi.com/api/activity")
-.then(function(response){
-    //console.log(response.status)
-    return response.json()
-}).then(function(data){
-    var suggestion = data.activity
-    console.log(suggestion)
-    //do something with the data
-})
+function getCategory(){
+    var type
+    var dropdownHeaderText = dropdownHeader.innerText
+    if (dropdownHeaderText === "Dropdown"){
+        type = ''
+    } else {
+        //the API query takes the category in lowercase
+        dropdownHeaderText = dropdownHeaderText[0].toLowerCase() + dropdownHeaderText.substring(1)
+        if (dropdownHeaderText === "d.I.Y."){
+            dropdownHeaderText = "diy"
+        }
+        type = '?type=' + dropdownHeaderText
+        console.log(type)
+    }
+    return type
+}
 
-//the call to giphy API
-//'q' is where the suggestion from bored API will go
-//all others are unimportant
-//for now 'q' is set to 'test'
-fetch("https://api.giphy.com/v1/gifs/search?api_key=PRySORNjXGxSwtXPYa9rBLmB9WAFuRDa&q=test&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
-.then(function(response){
-    //console.log(response.status)
-    return response.json()
-}).then(function(data){
-    var GIF = data.data[0]
-    var GIFImage = GIF.images.original.url
-    console.log(GIFImage)
-})
+function getThingToDo(){
+    var type = getCategory()
+    
+    fetch("https://www.boredapi.com/api/activity" + type)
+    .then(function(response){
+        //console.log(response.status)
+        return response.json()
+    }).then(function(data){
+        var suggestion = data.activity
+        boredAPIResponse.innerHTML = suggestion
+        //do something with the data
+        getGIF(suggestion)
+    })
+}
+
+function getGIF(query){
+    //the call to giphy API
+    //'q' is where the suggestion from bored API will go
+    //all others are unimportant
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=PRySORNjXGxSwtXPYa9rBLmB9WAFuRDa&q=" + query + "&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips")
+    .then(function(response){
+        //console.log(response.status)
+        return response.json()
+    }).then(function(json){
+        var GIF = json.data[0]
+        var GIFImage = GIF.images.original.url
+        //console.log(GIFImage)
+        GIFEl.src = GIFImage
+    })
+}
 
 function selectOption(event){
     var selectedOption = event.target.innerText
@@ -33,6 +60,7 @@ function selectOption(event){
 }
 
 //event listener for the dropdown to select which option the user wants
+mainButton.addEventListener("click", getThingToDo)
 dropdownOptions.addEventListener("click", selectOption)
 
 document.addEventListener('DOMContentLoaded', () => {
